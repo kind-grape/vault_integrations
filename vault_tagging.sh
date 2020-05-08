c153978ca919e75ceb20ace43a93415116434dc9
kind-grape <penglizhuo@gmail.com>
2020-05-08 15:10:16 -0400

    Added sudo for aws command incase sudo permission is required

--- vault_tagging.sh
+++ vault_tagging.sh
@@ -3,8 +3,8 @@ export region=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/
 export is_active=$(curl --silent -k $VAULT_ADDR/v1/sys/leader | jq -r .is_self)
 if if_active==true; then
     echo 'this is active node, tag second'
-    aws ec2 create-tags --region $region --resources $instance_id --tags Key='Patch Group',Value='prd-rhel-security-critical-second'
+    sudo aws ec2 create-tags --region $region --resources $instance_id --tags Key='Patch Group',Value='prd-rhel-security-critical-second'
 else
     echo 'this is passive node, tag first'
-    aws ec2 create-tags --region $region --resources $instance_id --tags Key='Patch Group',Value='prd-rhel-security-critical-first'
+    sudo aws ec2 create-tags --region $region --resources $instance_id --tags Key='Patch Group',Value='prd-rhel-security-critical-first'
 fi
